deb57e9b2d84b4c9318658d83d47ccccf1112279
GiantSox <thegiantsox@gmail.com>
2016-06-18 18:59:08 -0700

    add scripts

---
+++ Assets/Scripts/PlatformSlider.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformSlider : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
+		transform.Translate (new Vector3 (1, 0, 0) * Time.deltaTime)
+	}
+}
