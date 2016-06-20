f1064addbb6549d5029f6e1b800acc804d54f6d8
David Goodman <thegiantsox@gmail.com>
2016-06-20 09:21:21 -0700

    Now VR sickness simulator!

---
+++ Assets/Scripts/Ballmove.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ballmove : MonoBehaviour {
+	private Rigidbody rb;
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void FixedUpdate () {
+		Vector3 movement = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
+		rb.AddForce (speed * movement);
+	}
+}
--- Assets/Scripts/PlatformSlider.cs
+++ Assets/Scripts/PlatformSlider.cs
@@ -3,14 +3,9 @@ using System.Collections;
 
 public class PlatformSlider : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
 	// Update is called once per frame
 	void Update () {
 		//transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
-		transform.Translate (new Vector3 (1, 0, 0) * Time.deltaTime)
+		transform.Translate (new Vector3 (-1, 0, 0) * Time.deltaTime);
 	}
 }
